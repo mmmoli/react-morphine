0da67e55e8ca2a7e5ed2418424774e9e9dc9f8e5
Michele Memoli <michele@100shapes.com>
2016-01-19 21:18:44 +0000

    Added test to check that hardware acceleration is forced for rendering

--- test/index.js
+++ test/index.js
@@ -91,3 +91,24 @@ test('SVGPath custom preserveAspectRatio attr', assert => {
 
   assert.end();
 });
+
+test('SVGPath forces CSS hardware acceleration', assert => {
+
+  const renderer = TestUtils.createRenderer();
+  renderer.render(
+    <SVGPath defaultStyle={{}}
+             style={{}}
+             width={10}
+             path={() => `M0,20 L220,40 L220,300 L0,300 Z`}
+             height={20} />
+  );
+  const output = renderer.getRenderOutput();
+
+  const actual = output.props.children().props.style.transform;
+  const expected = 'translate3d(0, 0, 0)';
+
+  assert.equal(actual, expected,
+    'Should render a transform3d style to force hardware acceleration');
+
+  assert.end();
+});
