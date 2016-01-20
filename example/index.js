44bf1efe9da8bcda5d55f66ecbb56f4bea8833f8
Michele Memoli <michele@100shapes.com>
2016-01-20 22:05:26 +0000

    Improved styling of example

--- example/index.js
+++ example/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './Card';
 
-ReactDOM.render(<Card />, document.querySelector('#content'));
+ReactDOM.render(<Card />, document.querySelector('#root'));
